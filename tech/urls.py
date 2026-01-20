3c9b243c234f18fde167457f53d9b5c7e466a8e6
Txagouuu <tiagomendon45@gmail.com>
2026-01-20 16:05:01 -0300

    feat: adiciona página de membros protegida por login

--- tech/urls.py
+++ tech/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from tech.views import index
+from tech.views import members
 
 urlpatterns = [
     path('', index, name='index'),
-
+    path('members/', members, name='members'),
 
 ]
